b158a7858a7534189909f4203a78956a03aac924
Xiaolong <xiaolong@cse.tamu.edu>
2015-06-18 11:07:32 -0400

    Conditionally create thumbnail for ios

--- src/ios/ImageResizer.m
+++ src/ios/ImageResizer.m
@@ -27,55 +27,60 @@
     CGFloat scaleFactor = 0.0;
     CGSize scaledSize = frameSize;
 
-    // calculate the aspect ratio if the image is to large
-    if (CGSizeEqualToSize(imageSize, frameSize) == NO) {
+    CDVPluginResult* result = nil;
+    if (width < targetWidth + 60 && height < targetHeight + 60) {
+        result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:imageUrlString];
+    } else {
+      // calculate the aspect ratio if the image is to large
+      if (CGSizeEqualToSize(imageSize, frameSize) == NO) {
         CGFloat widthFactor = targetWidth / width;
         CGFloat heightFactor = targetHeight / height;
 
         // opposite comparison to imageByScalingAndCroppingForSize in order to contain the image within the given bounds
         if (widthFactor > heightFactor) {
-            scaleFactor = heightFactor; // scale to fit height
+	  scaleFactor = heightFactor; // scale to fit height
         } else {
-            scaleFactor = widthFactor; // scale to fit width
+	  scaleFactor = widthFactor; // scale to fit width
         }
         scaledSize = CGSizeMake(MIN(width * scaleFactor, targetWidth), MIN(height * scaleFactor, targetHeight));
-    }
+      }
 
-    // If the pixels are floats, it causes a white line in iOS8 and probably other versions too
-    scaledSize.width = (int)scaledSize.width;
-    scaledSize.height = (int)scaledSize.height;
+      // If the pixels are floats, it causes a white line in iOS8 and probably other versions too
+      scaledSize.width = (int)scaledSize.width;
+      scaledSize.height = (int)scaledSize.height;
 
-    UIGraphicsBeginImageContext(scaledSize); // this will resize
+      UIGraphicsBeginImageContext(scaledSize); // this will resize
 
-    [sourceImage drawInRect:CGRectMake(0, 0, scaledSize.width, scaledSize.height)];
+      [sourceImage drawInRect:CGRectMake(0, 0, scaledSize.width, scaledSize.height)];
 
-    newImage = UIGraphicsGetImageFromCurrentImageContext();
-    if (newImage == nil) {
+      newImage = UIGraphicsGetImageFromCurrentImageContext();
+      if (newImage == nil) {
         NSLog(@"could not scale image");
-    }
+      }
 
-    // pop the context to get back to the default
-    UIGraphicsEndImageContext();
+      // pop the context to get back to the default
+      UIGraphicsEndImageContext();
 
-    // get the temp directory path
-    NSString* docsPath = [NSTemporaryDirectory()stringByStandardizingPath];
-    NSError* err = nil;
-    NSFileManager* fileMgr = [[NSFileManager alloc] init];
+      // get the temp directory path
+      NSString* docsPath = [NSTemporaryDirectory()stringByStandardizingPath];
+      NSError* err = nil;
+      NSFileManager* fileMgr = [[NSFileManager alloc] init];
 
-    // generate unique file name
-    NSString* filePath;
-    NSData* data = UIImageJPEGRepresentation(newImage, [quality floatValue] / 100.0f);
-    int i = 1;
-    do {
+      // generate unique file name
+      NSString* filePath;    
+      NSData* data = UIImageJPEGRepresentation(newImage, [quality floatValue] / 100.0f);
+      int i = 1;
+      do {
         filePath = [NSString stringWithFormat:@"%@/%@%03d.%@", docsPath, PROTONET_PHOTO_PREFIX, i++, @"jpg"];
-    } while ([fileMgr fileExistsAtPath:filePath]);
+      } while ([fileMgr fileExistsAtPath:filePath]);
 
-    // save file
-    CDVPluginResult* result = nil;
-    if (![data writeToFile:filePath options:NSAtomicWrite error:&err]) {
+      // save file
+
+      if (![data writeToFile:filePath options:NSAtomicWrite error:&err]) {
         result = [CDVPluginResult resultWithStatus:CDVCommandStatus_IO_EXCEPTION messageAsString:[err localizedDescription]];
-    } else {
+      } else {
         result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:[[NSURL fileURLWithPath:filePath] absoluteString]];
+      }
     }
     [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
 }
