b0df0ac66ac946b150925f6229ca108140416abe
Xiaolong <xiaolong@cse.tamu.edu>
2016-06-15 11:27:09 -0400

    Support for passed fileName

--- src/ios/ImageResizer.m
+++ src/ios/ImageResizer.m
@@ -15,7 +15,7 @@
     NSString* quality = [arguments objectForKey:@"quality"];
     CGSize frameSize = CGSizeMake([[arguments objectForKey:@"width"] floatValue], [[arguments objectForKey:@"height"] floatValue]);
     NSString* fileName = [arguments objectForKey:@"fileName"];
-
+    
     //Get the image from the path
     NSURL* imageURL = [NSURL URLWithString:imageUrlString];
     UIImage* sourceImage = [UIImage imageWithData: [NSData dataWithContentsOfURL: imageURL]];
@@ -27,61 +27,66 @@
     CGFloat targetHeight = frameSize.height;
     CGFloat scaleFactor = 0.0;
     CGSize scaledSize = frameSize;
-
+    
     CDVPluginResult* result = nil;
     if (width < targetWidth + 60 && height < targetHeight + 60) {
         result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:imageUrlString];
     } else {
-      // calculate the aspect ratio if the image is to large
-      if (CGSizeEqualToSize(imageSize, frameSize) == NO) {
-        CGFloat widthFactor = targetWidth / width;
-        CGFloat heightFactor = targetHeight / height;
-
-        // opposite comparison to imageByScalingAndCroppingForSize in order to contain the image within the given bounds
-        if (widthFactor > heightFactor) {
-	  scaleFactor = heightFactor; // scale to fit height
-        } else {
-	  scaleFactor = widthFactor; // scale to fit width
+        // calculate the aspect ratio if the image is to large
+        if (CGSizeEqualToSize(imageSize, frameSize) == NO) {
+            CGFloat widthFactor = targetWidth / width;
+            CGFloat heightFactor = targetHeight / height;
+            
+            // opposite comparison to imageByScalingAndCroppingForSize in order to contain the image within the given bounds
+            if (widthFactor > heightFactor) {
+                scaleFactor = heightFactor; // scale to fit height
+            } else {
+                scaleFactor = widthFactor; // scale to fit width
+            }
+            scaledSize = CGSizeMake(MIN(width * scaleFactor, targetWidth), MIN(height * scaleFactor, targetHeight));
         }
-        scaledSize = CGSizeMake(MIN(width * scaleFactor, targetWidth), MIN(height * scaleFactor, targetHeight));
-      }
-
-      // If the pixels are floats, it causes a white line in iOS8 and probably other versions too
-      scaledSize.width = (int)scaledSize.width;
-      scaledSize.height = (int)scaledSize.height;
-
-      UIGraphicsBeginImageContext(scaledSize); // this will resize
-
-      [sourceImage drawInRect:CGRectMake(0, 0, scaledSize.width, scaledSize.height)];
-
-      newImage = UIGraphicsGetImageFromCurrentImageContext();
-      if (newImage == nil) {
-        NSLog(@"could not scale image");
-      }
-
-      // pop the context to get back to the default
-      UIGraphicsEndImageContext();
-
-      // get the temp directory path
-      NSString* docsPath = [NSTemporaryDirectory()stringByStandardizingPath];
-      NSError* err = nil;
-      NSFileManager* fileMgr = [[NSFileManager alloc] init];
-
-      // generate unique file name
-      NSString* filePath;    
-      NSData* data = UIImageJPEGRepresentation(newImage, [quality floatValue] / 100.0f);
-      int i = 1;
-      do {
-        filePath = [NSString stringWithFormat:@"%@/%@%03d.%@", docsPath, PROTONET_PHOTO_PREFIX, i++, @"jpg"];
-      } while ([fileMgr fileExistsAtPath:filePath]);
-
-      // save file
-
-      if (![data writeToFile:filePath options:NSAtomicWrite error:&err]) {
-        result = [CDVPluginResult resultWithStatus:CDVCommandStatus_IO_EXCEPTION messageAsString:[err localizedDescription]];
-      } else {
-        result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:[[NSURL fileURLWithPath:filePath] absoluteString]];
-      }
+        
+        // If the pixels are floats, it causes a white line in iOS8 and probably other versions too
+        scaledSize.width = (int)scaledSize.width;
+        scaledSize.height = (int)scaledSize.height;
+        
+        UIGraphicsBeginImageContext(scaledSize); // this will resize
+        
+        [sourceImage drawInRect:CGRectMake(0, 0, scaledSize.width, scaledSize.height)];
+        
+        newImage = UIGraphicsGetImageFromCurrentImageContext();
+        if (newImage == nil) {
+            NSLog(@"could not scale image");
+        }
+        
+        // pop the context to get back to the default
+        UIGraphicsEndImageContext();
+        
+        // get the temp directory path
+        NSString* docsPath = [NSTemporaryDirectory() stringByStandardizingPath];
+        NSError* err = nil;
+        NSFileManager* fileMgr = [[NSFileManager alloc] init];
+        
+        // generate unique file name
+        NSString* filePath;
+        NSData* data = UIImageJPEGRepresentation(newImage, [quality floatValue] / 100.0f);
+        
+        if (fileName == nil) {
+            int i = 1;
+            do {
+                filePath = [NSString stringWithFormat:@"%@/%@%03d.%@", docsPath, PROTONET_PHOTO_PREFIX, i++, @"jpg"];
+            } while ([fileMgr fileExistsAtPath:filePath]);
+        } else {
+            filePath = [NSString stringWithFormat:@"%@/%@", docsPath, fileName];
+        }
+        
+        // save file
+        
+        if (![data writeToFile:filePath options:NSAtomicWrite error:&err]) {
+            result = [CDVPluginResult resultWithStatus:CDVCommandStatus_IO_EXCEPTION messageAsString:[err localizedDescription]];
+        } else {
+            result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:[[NSURL fileURLWithPath:filePath] absoluteString]];
+        }
     }
     [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
 }
